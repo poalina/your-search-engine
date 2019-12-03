e47b538208bd46e6ac474af36d206119442358e3
poalina <paula828@op.pl>
2019-12-03 11:34:04 +0000

    adds pagination

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   Text,
   ScrollView,
   View,
+  Button,
   ActivityIndicator
 } from "react-native";
 import ArticleCard from "./Components/ArticleCard";
@@ -14,22 +15,41 @@ import SearchBox from "./Components/SearchBox";
 export default class App extends Component {
   state = {
     isLoading: true,
-    data: null
+    data: null,
+    err: null,
+    page: 1,
+    pages: null,
+    query: "orange"
   };
 
   componentDidMount() {
     this.fetchData();
   }
+  handleChangePage = direction => {
+    this.setState(currentState => {
+      return { page: currentState.page + direction };
+    });
+  };
 
   fetchData = () => {
-    let query = "pink";
-    let page = 4;
-    api
-      .getData(query, page)
-      .then(data => this.setState({ isLoading: false, data: data.results }));
+    const query = this.state.query;
+    const page = this.state.page;
+    api.getData(query, page).then(data =>
+      this.setState({
+        isLoading: false,
+        data: data.results,
+        pages: data.pages
+      })
+    );
   };
+  componentDidUpdate(prevProps, prevState) {
+    const { page } = this.state;
+    if (prevState.page !== page) {
+      this.fetchData();
+    }
+  }
   render() {
-    const { data, isLoading } = this.state;
+    const { data, isLoading, pages, page } = this.state;
     if (isLoading) {
       return (
         <View>
@@ -45,6 +65,18 @@ export default class App extends Component {
           {data.map(item => {
             return <ArticleCard key={item.id} item={item} />;
           })}
+          <View style={styles.buttons}>
+            <Button
+              disabled={page === 1}
+              onPress={() => this.handleChangePage(-1)}
+              title="Prev page"
+            />
+            <Button
+              disabled={page === pages}
+              onPress={() => this.handleChangePage(1)}
+              title="Next page"
+            />
+          </View>
         </ScrollView>
       </View>
     );
@@ -58,5 +90,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 25
+  },
+  buttons: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingTop: 20
   }
 });
